e68585e30af2d90440bde7a7461c95ff11915dd9
rudy <rudy@supertree.co>
2022-09-27 16:19:27 +0900

    PIDL.exe로 생성되는 .cpp, .h, .inl 파일을 git으로 관리합니다.
    - 맥에서는 해당 파일들을 생성하려면 복잡해짐

---
+++ engine/src/EmergencyS2C_common.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+namespace EmergencyS2C {
+
+
+    //Message ID that replies to each RMI method.
+    static const ::Proud::RmiID Rmi_EmergencyLogData_AckComplete = (::Proud::RmiID)(750+1);
+
+    // List that has RMI ID.
+    extern ::Proud::RmiID g_RmiIDList[];
+    // RmiID List Count
+    extern int g_RmiIDListCount;
+
+}
+
+
+
+// Forward declarations
+
+
+// Declarations
+
+
---
+++ engine/src/NetC2C_common.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CompactFieldMap.h"
+namespace ProudC2C {
+
+
+    //Message ID that replies to each RMI method.
+    static const ::Proud::RmiID Rmi_HolsterP2PHolepunchTrial = (::Proud::RmiID)(65000+1);
+    static const ::Proud::RmiID Rmi_ReportUdpMessageCount = (::Proud::RmiID)(65000+2);
+    static const ::Proud::RmiID Rmi_RoundTripLatencyPing = (::Proud::RmiID)(65000+3);
+    static const ::Proud::RmiID Rmi_RoundTripLatencyPong = (::Proud::RmiID)(65000+4);
+
+    // List that has RMI ID.
+    extern ::Proud::RmiID g_RmiIDList[];
+    // RmiID List Count
+    extern int g_RmiIDListCount;
+
+}
+
+
+
+// Forward declarations
+
+
+// Declarations
+
+
