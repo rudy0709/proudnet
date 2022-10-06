261475ca5621c8532746ea23360402c53f4f8a46
rudy <rudy@supertree.co>
2022-10-06 17:11:13 +0900

    IOS 플랫폼에서의 빌드 스크립트를 수정합니다.

--- engine/ProudNetClient-IOS/pn_build.sh
+++ engine/ProudNetClient-IOS/pn_build.sh
@@ -55,14 +55,14 @@ build_library() {
 	libtool -static ./Debug-iphoneos/libProudNetClient.a ./Debug-iphonesimulator/libProudNetClient.a -o ./Debug-universal/libProudNetClient.a
 
 	# 빌드 결과물을 복사합니다.
-	mkdir -p build/libs/Debug-iphoneos/ build/libs/Debug-iphonesimulator/ build/libs/Debug-universal/
-	mkdir -p build/libs/Release-iphoneos/ build/libs/Release-iphonesimulator/ build/libs/Release-universal/
-	mv -f Debug-iphoneos/libProudNetClient.a /build/libs/Debug-iphoneos/
-	mv -f Debug-iphonesimulator/libProudNetClient.a /build/libs/Debug-iphonesimulator/
-	mv -f Debug-universal/libProudNetClient.a /build/libs/Debug-universal/
-	mv -f Release-iphoneos/libProudNetClient.a /build/libs/Release-iphoneos/
-	mv -f Release-iphonesimulator/libProudNetClient.a /build/libs/Release-iphonesimulator/
-	mv -f Release-universal/libProudNetClient.a /build/libs/Release-universal/
+	mkdir -p ./build/libs/Debug-iphoneos/ ./build/libs/Debug-iphonesimulator/ ./build/libs/Debug-universal/
+	mkdir -p ./build/libs/Release-iphoneos/ ./build/libs/Release-iphonesimulator/ ./build/libs/Release-universal/
+	mv -f Debug-iphoneos/libProudNetClient.a ./build/libs/Debug-iphoneos/
+	mv -f Debug-iphonesimulator/libProudNetClient.a ./build/libs/Debug-iphonesimulator/
+	mv -f Debug-universal/libProudNetClient.a ./build/libs/Debug-universal/
+	mv -f Release-iphoneos/libProudNetClient.a ./build/libs/Release-iphoneos/
+	mv -f Release-iphonesimulator/libProudNetClient.a ./build/libs/Release-iphonesimulator/
+	mv -f Release-universal/libProudNetClient.a ./build/libs/Release-universal/
 	rm -rf Debug-*
 	rm -rf Release-*
 }
@@ -78,29 +78,5 @@ build_library_by_llvm() {
 			;;
 	esac
 }
-
-#build_library_all() {
-#	echo "build .................." #222-삭제:
-
-#	# ProudNetClient.xcodeproj 폴더가 존재하는 지를 검사합니다.
-#	proj_dir_path='./ProudNetClient.xcodeproj'
-#	if [ ! -d $proj_dir_path ]; then
-#		echo "'$proj_dir_path' 폴더를 cmake로 생성합니다."
-
-#		#sudo xcode-select --reset
-#		cmake -DCMAKE_C_COMPILER=`xcrun -find cc` -DCMAKE_CXX_COMPILER=`xcrun -find c++` -G Xcode ./
-#	fi
-
-#	# arm64, x86_64 아키텍처 용의 라이브러리를 각각 빌드합니다.
-#	build_library "Release" "iphoneos" "arm64"
-#	build_library "Debug" "iphoneos" "arm64"
-#	build_library "Release" "iphonesimulator" "arm64"
-#	build_library "Debug" "iphonesimulator" "arm64"
-
-#	# arm64, x86_64 아키텍처 용의 라이브러를 하나로 합칩니다.
-#	mkdir Release-universal-lib/ Debug-universal-lib/
-#	libtool -static ./Release-iphoneos/libProudNetClient.a ./Release-iphonesimulator/libProudNetClient.a -o ./Release-universal-lib/libProudNetClient.a
-#	libtool -static ./Debug-iphoneos/libProudNetClient.a ./Debug-iphonesimulator/libProudNetClient.a -o ./Debug-universal-lib/libProudNetClient.a
-#}
 
 main $1
