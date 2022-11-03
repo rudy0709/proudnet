952abb87a0ae58bc6ce07e7ca44dec95bbb209b7
rudy <rudy@supertree.co>
2022-11-03 12:21:31 +0900

    프라우드넷 버전을 관리하는 C# 파일을 공통 폴더(version\)로 옮깁니다.
    - 패키징 시에 "HERE_SHALL_BE_EDITED_BY_BUILD_HELPER" 문자열을 교체하는 방식은 사용 안함

---
+++ version/PNVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIDL
+{
+    class PNVersion
+    {
+        // 이 곳에 프라우드넷 버전을 입력해야 합니다. (HERE_SHALL_BE_EDITED_BY_BUILD_HELPER)
+        // 소스 저장소를 github으로 옮긴 이후로는 버전을 major.minor.patch 형식으로 관리합니다. (perforce에서의 마지막 버전은 1.7.56164 입니다.)
+        static public string g_versionText = "1.8.0";
+    }
+}
