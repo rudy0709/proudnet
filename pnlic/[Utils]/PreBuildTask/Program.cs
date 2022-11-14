3f573b39762999afa6e3925a49e64245fc344147
rudy <rudy@supertree.co>
2022-11-14 11:37:25 +0900

    오픈소스 및 nuget 패키지를 설치하는 스크립트를 추가합니다.

---
+++ pnlic/[Utils]/PreBuildTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PreBuildTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#### 이 프로젝트의 빌드 결과물(.exe)은 무시 하세요. ####");
+            Console.WriteLine("#### 오픈소스 및 nuget 패키지를 미리 설치하기 위한 프로젝트입니다. ####");
+            Console.WriteLine("  - nuget 오픈소스 다운로드");
+            Console.WriteLine("  - Antlr4 / Antlr4.CodeGenerator / Antlr4.Runtime / YamlDotNet 패키지 설치");
+            Console.WriteLine("  - CodeVirtualizer 오픈소스 다운로드");
+        }
+    }
+}
