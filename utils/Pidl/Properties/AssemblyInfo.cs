81d15d0b559072d34248b130aec40cfa435d0cb8
rudy <rudy@supertree.co>
2022-12-05 19:25:19 +0900

    PIDL 컴파일러 프로젝트의 설저을 변경합니다.

--- utils/Pidl/Properties/AssemblyInfo.cs
+++ utils/Pidl/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PIDL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PIDL Compiler")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PIDL")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Nettention Co., Ltd.")]
+[assembly: AssemblyProduct("ProudNet")]
+[assembly: AssemblyCopyright("Copyright © Nettention Co., Ltd. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
