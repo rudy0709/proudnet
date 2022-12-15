1900e814ab811f2053427f500852df3d0b178595
rudy <rudy@supertree.co>
2022-12-16 00:02:25 +0900

    프로젝트 설정파일들을 정리하고 솔루션 파일에서 빌드 순서를 확인할 수 있도록 프로젝트명에 번호를 붙입니다.

--- utils/utils_build.bat
+++ utils/utils_build.bat
@@ -1,21 +1,26 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-set projects=all code-virtualizer image-gen pidl-compiler
+set project_list=all image_gen pidl_compiler
+set msbuild_target=
+set msbuild_config_cpp=
+set msbuild_platform_cpp=
+set msbuild_config_cs=
+set msbuild_platform_cs=
+set msbuild_project=
 
 :main
 	@rem 사용법을 출력해야 할 지를 검사합니다.
-	call :check_comandline_params %1 %2
+	call :check_comandline_params %1 %2 %3 %4
 	if "%errorlevel%" == "1" (
-		@rem 파일 경로에서 파일명만 뽑아냅니다.
-		set filename=""
-		for %%i in ("%0") do (
-			set filename=%%~ni%%~xi
-		)
-
 		@rem 사용법을 출력합니다.
 		echo Usage:
-		echo     %filename% clean ^<all ^| code-virtualizer ^| image-gen ^| pidl-compiler^>
-		echo     %filename% build ^<all ^| code-virtualizer ^| image-gen ^| pidl-compiler^>
+		echo     pnlic_build.bat clean ^<configuration^> ^<platform^> ^<project^>
+		echo     pnlic_build.bat build ^<configuration^> ^<platform^> ^<project^>
+		echo Options:
+		echo     configuration : Debug ^| Debug_static_CRT ^| Release ^| Release_static_CRT
+		echo          platform : Win32 ^| x64
+		echo           project : all ^| pidl ^| authnet_lib ^| lic_auth_lib ^| pnlic_lib ^| pnlic_warn ^| pnlic_hidden ^| pnlic_auth ^| watermark
 		exit /b
 	)
 
@@ -23,14 +28,11 @@ set projects=all code-virtualizer image-gen pidl-compiler
 	@rem   (1) 환경변수 체크
 	call :check_environment_variable
 
-	@rem   (2) CodeVirtualizer 프로젝트 빌드
-	call :build_project_code_virtualizer %1 %2
+	@rem   (2) ImageGen 프로젝트 빌드
+	call :build_project_image_gen
 
-	@rem   (3) ImageGen 프로젝트 빌드
-	call :build_project_image_gen %1 %2
-
-	@rem   (4) Pidl 컴파일러 프로젝트 빌드
-	call :build_project_pidl_compiler %1 %2
+	@rem   (3) Pidl 컴파일러 프로젝트 빌드
+	call :build_project_pidl_compiler
 exit /b
 
 :check_environment_variable
@@ -53,88 +55,150 @@ exit /b
 		exit /b
 	)
 
-	echo ^>^>^>^> Environment-Variable^(PN_BUILD_PATH^) = "%PN_BUILD_PATH%"
-	echo ^>^>^>^> Environment-Variable^(PN_OBFUSCATION_TOOL_PATH^) = "%PN_OBFUSCATION_TOOL_PATH%"
-	echo ^>^>^>^> Environment-Variable^(PN_SIGN_TOOL_PATH^) = "%PN_SIGN_TOOL_PATH%"
+	echo ^>^>^>^> Env-Var = ^{
+	echo ^>^>^>^>              "PN_BUILD_PATH" : "%PN_BUILD_PATH%",
+	echo ^>^>^>^>   "PN_OBFUSCATION_TOOL_PATH" : "%PN_OBFUSCATION_TOOL_PATH%",
+	echo ^>^>^>^>           "PN_SIGN_TOOL_PATH": "%PN_SIGN_TOOL_PATH%"
+	echo ^>^>^>^> ^}
 	echo ^>^>^>^>
-exit /b
-
-:build_project_code_virtualizer
-	if not "%2" == "all" (
-		if not "%2" == "code-virtualizer" (
-			exit /b
-		)
-	)
-
-	if "%1" == "clean" (
-		@rem CodeVirtualizer 프로젝트의 빌드 시에 만들어진 폴더 및 파일들을 삭제합니다.
-		call :compile_command ".\CodeVirtualizer\CodeVirtualizer.vcxproj" clean Release_static_CRT x64
-		rd /s /q .\CodeVirtualizer\bin\
-		rd /s /q .\CodeVirtualizer\obj\
-	) else if "%1" == "build" (
-		@rem CodeVirtualizer 프로젝트를 빌드합니다.
-		call :compile_command ".\CodeVirtualizer\CodeVirtualizer.vcxproj" build Release_static_CRT x64
-	)
 exit /b
 
 :build_project_image_gen
-	if not "%2" == "all" (
-		if not "%2" == "image-gen" (
+	if not "%msbuild_project%" == "all" (
+		if not "%msbuild_project%" == "image_gen" (
 			exit /b
 		)
 	)
 
-	if "%1" == "clean" (
-		@rem ImageGen 프로젝트의 빌드 시에 만들어진 폴더 및 파일들을 삭제합니다.
-		call :compile_command ".\ImageGen\ImageGen.vcxproj" clean Release_static_CRT x64
-		rd /s /q .\ImageGen\bin\
-		rd /s /q .\ImageGen\obj\
-	) else if "%1" == "build" (
-		@rem ImageGen 프로젝트를 빌드합니다.
-		call :compile_command ".\ImageGen\ImageGen.vcxproj" build Release_static_CRT x64
-	)
+	@rem ImageGen 프로젝트들을 빌드/클린합니다.
+	call :compile_command ".\ImageGen" "ImageGen.vcxproj"
 exit /b
 
 :build_project_pidl_compiler
-	if not "%2" == "all" (
-		if not "%2" == "pidl-compiler" (
+	if not "%msbuild_project%" == "all" (
+		if not "%msbuild_project%" == "pidl_compiler" (
 			exit /b
 		)
 	)
 
-	if "%1" == "clean" (
-		@rem PIDL 컴파일러 프로젝트의 빌드 시에 만들어진 폴더 및 파일들을 삭제합니다.
-		call :compile_command ".\Pidl\Pidl.csproj" clean Release AnyCPU
-		rd /s /q .\Pidl\bin\
-		rd /s /q .\Pidl\obj\
-	) else if "%1" == "build" (
-		@rem PIDL 컴파일러 프로젝트를 빌드합니다.
-		call :compile_command ".\Pidl\Pidl.csproj" build Release AnyCPU
+	@rem PIDL 컴파일러 프로젝트를 빌드/클린합니다.
+	call :compile_command ".\Pidl" "2) Pidl.csproj"
+exit /b
+
+@rem 공통 로직...
+:check_comandline_params
+	@rem 첫번째 인자값의 유효성을 검사합니다.
+	set target_list=clean build
+	for %%i in (%target_list%) do (
+		if "%1" == "%%i" (
+			set msbuild_target=%%i
+		)
 	)
+
+	if "%msbuild_target%" == "" (
+		echo ^>^>^>^> Error : 1st^(Build Target^) argument is invalid.
+		exit /b 1
+	)
+
+	@rem 두번째 인자값의 유효성을 검사합니다.
+	set config_list=Debug Debug_static_CRT Release Release_static_CRT
+	for %%i in (%config_list%) do (
+		if "%2" == "%%i" (
+			set msbuild_config_cpp=%%i
+		)
+	)
+
+	if "%msbuild_config_cpp%" == "" (
+		echo ^>^>^>^> Error : 2nd^(Build Configuration^) argument is invalid.
+		exit /b 1
+	)
+
+	@rem 세번째 인자값의 유효성을 검사합니다.
+	set platform_list=Win32 x64
+	for %%i in (%platform_list%) do (
+		if "%3" == "%%i" (
+			set msbuild_platform_cpp=%%i
+		)
+	)
+
+	if "%msbuild_platform_cpp%" == "" (
+		echo ^>^>^>^> Error : 3rd^(Build Platform^) argument is invalid.
+		exit /b 1
+	)
+
+	@rem 세번째 인자값의 유효성을 검사합니다.
+	for %%i in (%project_list%) do (
+		if "%4" == "%%i" (
+			set msbuild_project=%%i
+		)
+	)
+
+	if "%msbuild_project%" == "" (
+		echo ^>^>^>^> Error : 4th^(Build Project^) argument is invalid.
+		exit /b 1
+	)
+
+	if not "%msbuild_config_cpp:Release=%" == "%msbuild_config_cpp%" (
+		set msbuild_config_cs=Release
+	) else (
+		set msbuild_config_cs=Debug
+	)
+	set msbuild_platform_cs=AnyCPU
+exit /b 0
+
+:check_environment_variable
+	@rem 환경변수가 등록되어 있는 지를 체크합니다.
+	if "%PN_BUILD_PATH%" == "" (
+		@rem 예시 : C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\MSBuild.exe
+		echo ^>^>^>^> Error : Register the PN_BUILD_PATH environment variable before executing the batch file.
+		exit /b
+	)
+
+	if "%PN_OBFUSCATION_TOOL_PATH%" == "" (
+		@rem 예시 : C:\Program Files (x86)\Gapotchenko\Eazfuscator.NET\Eazfuscator.NET.exe
+		echo ^>^>^>^> Error : Register the PN_OBFUSCATION_TOOL_PATH environment variable before executing the batch file.
+		exit /b
+	)
+
+	if "%PN_SIGN_TOOL_PATH%" == "" (
+		@rem 예시 : C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe
+		echo ^>^>^>^> Error : Register the PN_SIGN_TOOL_PATH environment variable before executing the batch file.
+		exit /b
+	)
+
+	echo ^>^>^>^> Env-Var = ^{
+	echo ^>^>^>^>              "PN_BUILD_PATH" : "%PN_BUILD_PATH%",
+	echo ^>^>^>^>   "PN_OBFUSCATION_TOOL_PATH" : "%PN_OBFUSCATION_TOOL_PATH%",
+	echo ^>^>^>^>           "PN_SIGN_TOOL_PATH": "%PN_SIGN_TOOL_PATH%"
+	echo ^>^>^>^> ^}
+	echo ^>^>^>^>
 exit /b
 
 :compile_command
+	set param1=%~1
+	set param2=%~2
 	set vs_version=17
 
-	echo ^>^>^>^> CommandLine : "%PN_BUILD_PATH%" %1 "/t:%2" "/p:Configuration=%3;Platform=%4;VisualStudioVersion=%vs_version%;BuildProjectReferences=false" /m
+	set msbuild_config=
+	set msbuild_platform=
+	if not "%param2:vcxproj=%" == "%param2%" (
+		set msbuild_config=!msbuild_config_cpp!
+		set msbuild_platform=!msbuild_platform_cpp!
+	) else if not "%param2:csproj=%" == "%param2%" (
+		set msbuild_config=!msbuild_config_cs!
+		set msbuild_platform=!msbuild_platform_cs!
+	)
+
+	echo ^>^>^>^> CommandLine : "%PN_BUILD_PATH%" "%param1%\%param2%" "/t:%msbuild_target%" "/p:Configuration=%msbuild_config%;Platform=%msbuild_platforms%;VisualStudioVersion=%vs_version%;BuildProjectReferences=false" /m
 	echo ^>^>^>^>
 	echo ^>^>^>^> --------------------------------------------------
-	"%PN_BUILD_PATH%" %1 "/t:%2" "/p:Configuration=%3;Platform=%4;VisualStudioVersion=%vs_version%;BuildProjectReferences=false" /m
+	"%PN_BUILD_PATH%" "%param1%\%param2%" "/t:%msbuild_target%" "/p:Configuration=%msbuild_config%;Platform=%msbuild_platforms%;VisualStudioVersion=%vs_version%;BuildProjectReferences=false" /m
 	echo ^>^>^>^> --------------------------------------------------
+
+	if "%msbuild_target%" == "clean" (
+		rd /s /q "%param1%\bin\"
+		rd /s /q "%param1%\obj\"
+	)
 exit /b
 
-:check_comandline_params
-	if not "%1" == "clean" (
-		if not "%1" == "build" (
-			exit /b 1
-		)
-	)
-
-	for %%p in (%projects%) do (
-		if "%2" == "%%p" (
-			exit /b 0
-		)
-	)
-exit /b 1
-
-call :main %1 %2
+call :main
