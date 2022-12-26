b7f5d1d9338eb5b55cd2d1c2865b49fdb80025aa
rudy <rudy@supertree.co>
2022-12-26 17:02:48 +0900

    Simple 클라/서버 샘플을 추가합니다.

---
+++ sample/Simple/Common/Simple_common.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+namespace Simple {
+
+
+	//Message ID that replies to each RMI method. 
+               
+    static const ::Proud::RmiID Rmi_Chat = (::Proud::RmiID)(2000+1);
+               
+    static const ::Proud::RmiID Rmi_ShowChat = (::Proud::RmiID)(2000+2);
+               
+    static const ::Proud::RmiID Rmi_SystemChat = (::Proud::RmiID)(2000+3);
+               
+    static const ::Proud::RmiID Rmi_P2PChat = (::Proud::RmiID)(2000+4);
+
+	// List that has RMI ID.
+	extern ::Proud::RmiID g_RmiIDList[];
+	// RmiID List Count
+	extern int g_RmiIDListCount;
+
+}
+
+
+ 
+
+// Forward declarations
+
+
+// Declarations
+
+
+
