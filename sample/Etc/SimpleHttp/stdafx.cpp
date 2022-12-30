d5317271a8e915c2aaeb82f6e10199d8b465e536
rudy <rudy@supertree.co>
2022-12-30 14:55:27 +0900

    HTTP 네트워크 샘플을 제거합니다.

--- sample/Etc/SimpleHttp/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-﻿// stdafx.cpp : Source file that contain only standard include file.
-// SampleHttpConnect_2008.pch become pre-compiled header.
-// stdafx.obj is included pre-compiled type information.
-
-#include "stdafx.h"
-
-// TODO: Additional require header refer STDAFX.H instead this file.
